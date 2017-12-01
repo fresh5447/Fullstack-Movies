1226df6f386c03d58d9fbf9df7fea19819df411b
fresh5447 <doug@kosmojo.com>
2017-11-30 20:31:53 -0700

    finish crud api for Movie model

--- server/server.js
+++ server/server.js
@@ -15,22 +15,59 @@ app.use(require('./config/error-handler'))
 
 const ourFirstMovie = {title: 'Jaws', year: 1965}
 
-Movie(ourFirstMovie).save((err, movie) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('MOVIE ADDED TO DB', movie)
-  }
+// Create A Movie
+app.post('/api/movies', (req, res) => {
+  const {title, year} = req.body
+  const newMovie = {title, year}
+  Movie(newMovie).save((err, savedMovie) => {
+    if (err) {
+      res.json({ error: err })
+    } else {
+      res.json({ msg: 'SUCCESS', data: savedMovie })
+    }
+  })
 })
 
-Movie.find((err, movies) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('****** HERES YOUR MOVIES ******', movies)
-  }
+//Find All Movies
+app.get('/api/movies', (req, res) => {
+  Movie.find((err, movies) => {
+    if (err) {
+      res.json({ error: err })
+    } else {
+      res.json({ msg: 'SUCCESS', movies })
+    }
+  })
 })
 
-const server = app.listen(port, () => console.log(`Running on port: ${port}`))
+app.get('/api/movies/:movieId', (req,res) => {
+
+  const movieId = req.params.movieId
+
+  const animal = {id: 123, name: 'Boots'}
+  Movie.findById({_id: movieId}, (err, movie) => {
+    if(err) {
+      res.json({ error: err })
+    } else {
+      res.json({ msg: `Found ${movie.title}`, movie: movie })
+    }
+  })
+
+})
+
+app.get('/api/random-movie', (req, res) => {
+  Movie.find((err, data) => {
+    if(err) {
+      res.json({ error: err})
+    } else {
+      const allMoviesArray = data
+      const randomItem = allMoviesArray[Math.floor(Math.random() * allMoviesArray.length)]
+      
+      res.json({msg: 'Random item Retrieved', data: randomItem})
+    }
+  })
+})
+
+
+const server = app.listen(port, () => console.log(`🔥🔥 Running on port: ${port} 🔥🔥`))
 
 module.exports = server
